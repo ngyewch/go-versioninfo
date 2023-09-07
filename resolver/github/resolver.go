4a00c98dc4642ed9c84c50c88ae9841708cc370a
Nick Ng <ngyewch@gmail.com>
2023-09-07 22:28:51 +0800

    fix: github resolver

--- resolver/github/resolver.go
+++ resolver/github/resolver.go
@@ -48,25 +48,30 @@ func (resolver *Resolver) Resolve() (*model.VersionInfo, error) {
 		info.Tag = ref[len(tagPrefix):]
 	}
 
-	head, err := resolver.repo.Head()
-	if err != nil {
-		return nil, err
-	}
+	// TODO Find other ways to get commit timestamp
+	_ = func() error {
+		head, err := resolver.repo.Head()
+		if err != nil {
+			return err
+		}
 
-	cIter, err := resolver.repo.Log(&git.LogOptions{
-		From:  head.Hash(),
-		Order: git.LogOrderCommitterTime,
-	})
-	if err != nil {
-		return nil, err
-	}
+		cIter, err := resolver.repo.Log(&git.LogOptions{
+			From:  head.Hash(),
+			Order: git.LogOrderCommitterTime,
+		})
+		if err != nil {
+			return err
+		}
 
-	c, err := cIter.Next()
-	if err != nil {
-		return nil, err
-	}
+		c, err := cIter.Next()
+		if err != nil {
+			return err
+		}
 
-	info.CommitTimestamp = c.Committer.When.Unix()
+		info.CommitTimestamp = c.Committer.When.Unix()
+
+		return nil
+	}()
 
 	return resolver.formatter.Format(info), nil
 }
